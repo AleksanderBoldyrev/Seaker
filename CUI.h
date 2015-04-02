fa16fcfd414bf4e40194d3120a630966676a93e6
AleksanderBoldyrev <aleksander.boldyrev@gmail.com>
2015-04-02 15:36:04 +0300

    Update CUI.h

--- CUI.h
+++ CUI.h
@@ -25,31 +25,27 @@ public:
 	void 		PrintField(Field& f);
 	void		PrintGame(Field& cf, Field& uf);
 
-	void 		InitFields();
-	void 		InitAI();
-	void 		UpdateFields();
-	void 		StartGame();
-	void 		EndGame();
-				CUI(units w, units h);
-				~CUI();
+			CUI(units w, units h);
+			~CUI();
+			
 	units		x = 10, y = 10;
 
 	const short	Black = 0x00,
-				Blue = 0x01,
-				Green = 0x02,
-				Cyan = 0x03,
-				Red = 0x04,
-				Magenta = 0x05,
-				Brown = 0x06,
-				LightGray = 0x07,
-				DarkGray = 0x08,
-				LightBlue = 0x09,
-				LightGreen = 0x0A,
-				LightCyan = 0x0B,
-				LightRed = 0x0C,
-				LightMag = 0x0D,
-				Yellow = 0x0E,
-				White = 0x0F;
+			Blue = 0x01,
+			Green = 0x02,
+			Cyan = 0x03,
+			Red = 0x04,
+			Magenta = 0x05,
+			Brown = 0x06,
+			LightGray = 0x07,
+			DarkGray = 0x08,
+			LightBlue = 0x09,
+			LightGreen = 0x0A,
+			LightCyan = 0x0B,
+			LightRed = 0x0C,
+			LightMag = 0x0D,
+			Yellow = 0x0E,
+			White = 0x0F;
 private:
 
 	units		_w, _h;

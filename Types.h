f6dffa512b5f9c5ce5a09e4c85bb09b3bf2b3953
AleksanderBoldyrev <aleksander.boldyrev@gmail.com>
2015-04-02 15:17:28 +0300

    Types.h
    The file consisting of the types used in the project.

---
+++ Types.h
@@ -0,0 +1,12 @@
+#ifndef _TYPES_H_
+#define _TYPES_H_
+
+typedef unsigned char 	cstate;		//Cell states.
+typedef unsigned short 	units;		//Units: cell counting.
+typedef unsigned char 	sstate;		//Ship states.
+typedef char 			dir; 		//Direction: horizontal ('h') and vertical ('v').
+
+const bool PLAYER = true;
+const bool COMP = false;
+
+#endif

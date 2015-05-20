f93018464ba97f416612a837bf745d654d7d39e6
AleksanderBoldyrev <aleksander.boldyrev@gmail.com>
2015-05-20 17:48:11 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,3 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <conio.h>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
 #include <iostream>
 
 using namespace std;
@@ -7,23 +13,9 @@ using namespace std;
 #include "Player.h"
 #include "CUI.h"
 #include "Field.h"
-#include "Ship.h"
 
-unsigned short shipspl[5];
-unsigned short shipsai[5];
-
-bool PlaceShips(CUI clCUI, AI art, Field& fuf, Field& fcf)
+bool PlaceShips(CUI* clCUI, AI* art, Field* fuf, Field* fcf)
 {
-	shipspl[0] = 10;
-	shipspl[1] = 4;
-	shipspl[2] = 3;
-	shipspl[3] = 2;
-	shipspl[4] = 1;
-	shipsai[0] = 10;
-	shipsai[1] = 4;
-	shipsai[2] = 3;
-	shipsai[3] = 2;
-	shipsai[4] = 1;
 	system("cls");
 	printf("Now you should arrange your ships on the board.");
 	bool sucplace = false;
@@ -36,11 +28,13 @@ bool PlaceShips(CUI clCUI, AI art, Field& fuf, Field& fcf)
 					count3 = 2,
 					count4 = 1,
 					currc  = 0;
-	clCUI.PrintGame(fuf, fcf);
-	clCUI.SetCursor(fuf, clCUI.x + 1, clCUI.y + 1);
-	clCUI.SetAim(fuf);
-	fuf.posx = 0;
-	fuf.posy = 0;
+	clCUI->PrintGame(fuf, fcf);
+	clCUI->SetCursor(fuf, clCUI->x + 1, clCUI->y + 1);
+	clCUI->SetAim(fuf);
+	clCUI->ChangeDir('h');
+	clCUI->ChangeLen(1);
+	fuf->posx = 0;
+	fuf->posy = 0;
 	while (currcc > 0)
 	{
 		while (!_kbhit());
@@ -48,28 +42,32 @@ bool PlaceShips(CUI clCUI, AI art, Field& fuf, Field& fcf)
 		switch (key)
 		{
 		case (0x77) :					//*** 'w' ***
-			clCUI.MoveAim('u', fuf, PLAYER);
+			clCUI->MoveAim('u', fuf, PLAYER);
 			break;
 		case (0x61) :					//*** 'a' ***
-			clCUI.MoveAim('l', fuf, PLAYER);
+			clCUI->MoveAim('l', fuf, PLAYER);
 			break;
 		case (0x73) :					//*** 's' ***
-			clCUI.MoveAim('d', fuf, PLAYER);
+			clCUI->MoveAim('d', fuf, PLAYER);
 			break;
 		case (0x64) :					//*** 'd' ***
-			clCUI.MoveAim('r', fuf, PLAYER);
+			clCUI->MoveAim('r', fuf, PLAYER);
 			break;
 		case (0x31) :					//*** '1' - length ***
 			len = 1;
+			clCUI->ChangeLen(1);
 			break;
 		case (0x32) :					//*** '2' - length ***
 			len = 2;
+			clCUI->ChangeLen(2); 
 			break;
 		case (0x33) :					//*** '3' - length ***
 			len = 3;
+			clCUI->ChangeLen(3); 
 			break;
 		case (0x34) :					//*** '4' - length ***
 			len = 4;
+			clCUI->ChangeLen(4);
 			break;
 		case (0x20) :					//*** ' ' ***			
 			switch (len)
@@ -111,7 +109,7 @@ bool PlaceShips(CUI clCUI, AI art, Field& fuf, Field& fcf)
 				else continue;
 				break;
 			}
-			if (!fuf.CreateShip(fuf.posx, fuf.posy, len, sd)) switch (len)
+			if (!fuf->CreateShip(fuf->posx, fuf->posy, len, sd)) switch (len)
 			{
 				case(1) :
 					count1++;
@@ -132,28 +130,51 @@ bool PlaceShips(CUI clCUI, AI art, Field& fuf, Field& fcf)
 			}
 			else
 			{
+				switch (len)
+				{
+				case(1) :
+					clCUI->AddShipState(PLAYER, 1, 10);
+					break;
+				case(2) :
+					clCUI->AddShipState(PLAYER, 2, 10);
+					break;
+				case(3) :
+					clCUI->AddShipState(PLAYER, 3, 10);
+					break;
+				case(4) :
+					clCUI->AddShipState(PLAYER, 4, 10);
+					break;
+				}
 				switch (sd)
 				{
 				case ('h') :
 					for (int i = 1; i != len + 1; i++)
 					{
 						Sleep(300);
-						clCUI.MoveAim('r', fuf, PLAYER);
+						clCUI->MoveAim('r', fuf, PLAYER);
 					}
 					break;
 				case ('v') :
 					for (int i = 1; i != len + 1; i++)
 					{
 						Sleep(300);
-						clCUI.MoveAim('d', fuf, PLAYER);
+						clCUI->MoveAim('d', fuf, PLAYER);
 					}
 					break;
 				}
 			}
 			break;
 		case (0x72) :					//*** 'r' ***
-			if (sd == 'h') sd = 'v';
-			else if (sd == 'v') sd = 'h';
+			if (sd == 'h')
+			{
+				sd = 'v';
+				clCUI->ChangeDir('v');
+			}
+			else if (sd == 'v')
+			{
+				sd = 'h';
+				clCUI->ChangeDir('h');
+			}
 			break;
 		case (0x78) :					//*** 'x' ***
 			exit(0);
@@ -164,51 +185,44 @@ bool PlaceShips(CUI clCUI, AI art, Field& fuf, Field& fcf)
 	return sucplace;
 }
 
-unsigned short MakeMove(CUI clCUI, AI art, Field& fuf, Field& fcf, unsigned short cellcount)
+void MakeMove(CUI* clCUI, AI* art, Field* fuf, Field* fcf)
 {
-
 	bool isturn = true;
 	unsigned short key;
-	clCUI.GotoXY(2 + fuf.Width(), 18 + fuf.Height());
-	printf("Now is the turn of the player.");
-	clCUI.SetCursor(fcf, fuf.Width() + clCUI.x + 2, clCUI.y + 1);
-	clCUI.SetAim(fcf);
-	fcf.posx = 0;
-	fcf.posy = 0;
-	while (cellcount > 0 && isturn)
+	unsigned short clen;
+	clCUI->SetCursor(fcf, fuf->Width() + clCUI->x + 2, clCUI->y + 1);
+	clCUI->SetAim(fcf);
+	fcf->posx = 0;
+	fcf->posy = 0;
+	while (fcf->shipsnum[0] > 0 && isturn)
 	{
 		while (!_kbhit());
 		key = _getch();
-		cstate st = fcf.GetPosVal();
+		cstate st = fcf->GetPosVal();
 		switch (key)
 		{
 		case (0x77) :					//*** 'w' ***
-			clCUI.MoveAim('u', fcf, COMP);
+			clCUI->MoveAim('u', fcf, COMP);
 			break;
 		case (0x61) :					//*** 'a' ***
-			clCUI.MoveAim('l', fcf, COMP);
+			clCUI->MoveAim('l', fcf, COMP);
 			break;
 		case (0x73) :					//*** 's' ***
-			clCUI.MoveAim('d', fcf, COMP);
+			clCUI->MoveAim('d', fcf, COMP);
 			break;
 		case (0x64) :					//*** 'd' ***
-			clCUI.MoveAim('r', fcf, COMP);
+			clCUI->MoveAim('r', fcf, COMP);
 			break;
 		case (0x66) :					//*** 'f' ***
-			cellcount = fcf.Fire(fcf.posx, fcf.posy, cellcount);
-			if (st != fcf.water && st != fcf.hit && st != fcf.miss)
+			clen = fcf->Fire(fcf->posx, fcf->posy);
+			if (st != fcf->water && st != fcf->hit && st != fcf->miss)
 			{
-				clCUI.GotoXY(fcf.posx + clCUI.x + fcf.Width() + 2, fcf.posy + clCUI.y + 1);
-				clCUI.SetColor(clCUI.Yellow, clCUI.LightBlue);
-				printf("%c", fcf.hit);
-				clCUI.SetColor(clCUI.White, clCUI.Black);
+				clCUI->PaintHM (true,  COMP, fcf);
+				if (clen != 0) clCUI->RemShipState(COMP, clen, 10);
 			}
-			else if (st == fcf.water)
+			else if (st == fcf->water)
 			{
-				clCUI.GotoXY(fcf.posx + clCUI.x + fcf.Width() + 2, fcf.posy + clCUI.y + 1);
-				clCUI.SetColor(clCUI.LightCyan, clCUI.LightBlue);
-				printf("%c", fcf.miss);
-				clCUI.SetColor(clCUI.White, clCUI.Black);
+				clCUI->PaintHM (false, COMP, fcf);
 				isturn = false;
 			}
 			break;
@@ -218,16 +232,16 @@ unsigned short MakeMove(CUI clCUI, AI art, Field& fuf, Field& fcf, unsigned shor
 		default: continue;
 		}
 	}
-	return cellcount;
 }
 
-bool PlayTheGame(CUI clCUI, AI art, Field& fiu, Field& fic, bool turn)
+bool PlayTheGame(CUI* clCUI, AI* art, Field* fiu, Field* fic, bool turn)
 {
+	clCUI->ChangeSet();
+	clCUI->ChangeTurnState(1);
 	srand((unsigned)time(0));
 	system("cls");
 	bool pturn_is_first = true;
 	bool aturn_is_first = true;
-	unsigned short pcellcount = 20, ccellcount = 20;
 	bool win = 0;
 	while (true)
 	{
@@ -235,25 +249,34 @@ bool PlayTheGame(CUI clCUI, AI art, Field& fiu, Field& fic, bool turn)
 		{
 		case (1) :
 			Sleep(300);
-			if (pturn_is_first) PlaceShips(clCUI, art, fiu, fic);
-			pcellcount = MakeMove(clCUI, art, fiu, fic, pcellcount);
-			pturn_is_first = false;
+			if (pturn_is_first)	
+			{
+				PlaceShips(clCUI, art, fiu, fic);
+				pturn_is_first = false;
+			}
+			else MakeMove(clCUI, art, fiu, fic);
 			turn = 0;
+			clCUI->ChangeTurnState(0);
 			break;
 		case (0) :
 			Sleep(300);
-			if (aturn_is_first) art.PlaceShipsAI(fic);
-			ccellcount = art.MakeMove(clCUI, art, fiu, fic, ccellcount);
-			aturn_is_first = false;
+			if (aturn_is_first) 
+			{
+				art->PlaceShipsAI(clCUI, fic);
+				aturn_is_first = false;
+				clCUI->ChangeState();
+			}
+			else art->MakeMove(clCUI, art, fiu, fic);
 			turn = 1;
+			clCUI->ChangeTurnState(1);
 			break;
 		}
-		if (pcellcount <= 0)
+		if (fic->shipsnum[0] <= 0)
 		{
 			win = 1;
 			break;
 		}
-		if (ccellcount <= 0)
+		if (fiu->shipsnum[0] <= 0)
 		{
 			win = 0;
 			break;
@@ -270,15 +293,15 @@ int main()
 
 	char* nam = new char[80];
 	Player pl1;
-	AI artint;
+	AI* artint = new AI();
 	printf("Enter your name: ");
 	gets(nam);
 	pl1.SetName(nam);
-	printf("Field of which length do you wish to create? (10..80) ");
+	printf("Field of which length do you wish to create? (10..25) ");
 	scanf("%d", &w);
-	while (w < 10 || w > 80)
+	while (w < 10 || w > 25)
 	{
-		printf("\nPlease enter a valid number: (10..80) ");
+		printf("\nPlease enter a valid number: (10..25) ");
 		scanf("%d", &w);
 	}
 	printf("Field of which height do you wish to create? (10..80) ");
@@ -288,21 +311,23 @@ int main()
 		printf("\nPlease enter a valid number: (10..80) ");
 		scanf("%d", &h);
 	}
-	printf("What type of AI do you want to play with? (1|0) ");
+	printf("What type of AI do you want to play with? (1 ~ hard | 0 ~ easy) ");
 	scanf("%d", &lv);
 	while (lv < 0 || lv > 1)
 	{
-		printf("\nPlease enter a valid number: (1|0) ");
+		printf("\nPlease enter a valid number: (1 ~ hard | 0 ~ easy) ");
 		scanf("%d", &lv);
 	}
-	artint.Setlevel(lv);
-	CUI   clGUI(w, h);
-	Field fc(COMP, w, h);
-	Field fu(PLAYER, w, h);
+	artint->Setlevel(lv);
+	CUI* clGUI = new CUI(w, h);
+	Field*  fc = new Field(COMP, w, h);
+	Field*  fu = new Field(PLAYER, w, h);
+	
+	clGUI->x = 40 - fu->Width() - 1;
 
 	//*** Here thy game begins ***
 
-	printf("=================== Here thy game begins! ===================\n\n\n");
+	printf("\n=================== Here thy game begins! ===================\n\n\n");
 
 	printf("Welcome to the \"Seaker\" game, %s!\n\n", pl1.GetName());
 	char reply;
@@ -320,13 +345,23 @@ int main()
 
 	while (reply == 'y')
 	{
+		fu->shipsnum[0] = 20;			//TODO: ships instead of cells.
+		fu->shipsnum[1] = 4;
+		fu->shipsnum[2] = 3;
+		fu->shipsnum[3] = 2;
+		fu->shipsnum[4] = 1;
+		fc->shipsnum[0] = 20;
+		fc->shipsnum[1] = 4;
+		fc->shipsnum[2] = 3;
+		fc->shipsnum[3] = 2;
+		fc->shipsnum[4] = 1;
 		system("cls");
-		fu.Wipe();
-		fc.Wipe();
+		fu->Wipe();
+		fc->Wipe();
 		
 		bool winner = PlayTheGame(clGUI, artint, fu, fc, turn);
-		clGUI.PrintGame(fu, fc);
-		printf("Press any key. ");
+		clGUI->PrintGame(fu, fc);
+		clGUI->ChangeState("Press key");
 		_getch();
 		system("cls");
 
@@ -351,6 +386,10 @@ int main()
 
 	//***Game Loop ends.
 
+	delete artint;
+	delete clGUI;
+	delete fc;
+	delete fu;
 	system("pause");
 	return 0;
 }

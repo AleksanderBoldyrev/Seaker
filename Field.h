abddc0205cb306d1342f99230e580783cf8492dd
AleksanderBoldyrev <aleksander.boldyrev@gmail.com>
2015-05-20 17:49:02 +0300

    Update Field.h

--- Field.h
+++ Field.h
@@ -12,40 +12,40 @@ using namespace std;
 
 class Field
 {
-	units   	 wcells, hcells;
+	units   				wcells, hcells;
 	vector<vector<cstate>>	cells;
-	void		 SetCell(units w, units h, cstate s);
-	cstate 		 GetCell(units w, units h);
-	units		 wc = 10, hc = 10;
-
+	units					wc = 10,				 hc = 10;
+	
+	void					SetCell					 (units w, units h, cstate s);
+	cstate 					GetCell					 (units w, units h);
+	unsigned short			Aftermath				 (units px, units py);
 public:
-	const cstate water = 0x20;
-	const cstate miss = 0x0F;
-	const cstate up = 0x1E;
-	const cstate down = 0x1F;
-	const cstate left = 0x11;
-	const cstate right = 0x10;
-	const cstate body = 0xFE;
-	const cstate onesh = 0x04;
-	const cstate hit = 0xB0;
+	const cstate			water					 = 0x20;
+	const cstate			miss					 = 0x0F;
+	const cstate			up						 = 0x1E;
+	const cstate			down					 = 0x1F;
+	const cstate			left					 = 0x11;
+	const cstate			right					 = 0x10;
+	const cstate			body					 = 0xFE;
+	const cstate			onesh					 = 0x04;
+	const cstate			hit						 = 0x06;
+	const cstate			err						 = 0xFF;
 
-	const cstate err = 0xFF;
+	unsigned short			shipsnum[5];
+	bool 					owner;											//True == Player; False == AI.
+	units					posx = 0,				 posy = 0;
 
-	bool 		 owner;											//True == Player; False == AI.
-	units		 posx = 0, posy = 0;
-				 Field(bool who, units sx, units sy);
-				 ~Field();
-	units		 Width() { return wcells; }
-	units		 Height() { return hcells; }
-	void		 Wipe();
-	bool 		 SetPos(units x, units y);
-	cstate		 GetPosVal();
-	void		 Fire(units px, units py);
-	void		 Miss(units px, units py);
-	bool		 CreateShip(units x, units y, units l, dir vdir);
-
-	bool		 ShipSpaceIsChecked(units x, units y, units l, dir vdir);
-	bool		 CenterCellSpaceIsChecked(units cellx, units celly, units le, dir di, units ord);
+							Field					 (bool who, units sx, units sy);
+							~Field					 ();
+	units					Width					 ()  { return wcells; }
+	units					Height					 ()  { return hcells; }
+	void					Wipe					 ();
+	bool 					SetPos					 (units x, units y);
+	cstate					GetPosVal				 ();
+	unsigned short			Fire					 (units px, units py);
+	bool					CreateShip				 (units x, units y, units l, dir vdir);
+	bool					ShipSpaceIsChecked		 (units x, units y, units l, dir vdir);
+	bool					CenterCellSpaceIsChecked (units cellx, units celly, units le, dir di, units ord);
 };
 
 #endif
